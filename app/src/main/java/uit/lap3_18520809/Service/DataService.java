29d49f05bbe08fa371ee8d9310c9978bd911e411
Quachbaohung1 <baohung9715@gmail.com>
2022-05-10 22:50:18 +0700

    Fix api

--- app/src/main/java/uit/lap3_18520809/Service/DataService.java
+++ app/src/main/java/uit/lap3_18520809/Service/DataService.java
@@ -9,7 +9,7 @@ import uit.labp3_18520809.Model.News;
 
 public interface DataService {
 
-    @GET("/v2/everything?q=apple&from=2022-04-29&to=2022-04-29&sortBy=popularity&apiKey=d9f20dfc50204b6cac20cccfeef1dbaa")
+    @GET("/v2/everything?q=tesla&from=2022-04-10&sortBy=publishedAt&apiKey=ebea7bae738444d897fe95db8ff13c23")
     Call<News> GetDataNews();
 
 }
